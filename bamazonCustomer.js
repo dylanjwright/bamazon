1be9b82c080112283a2c7f7ca4dbfc42c3f3efd4
Dylan Wright <wright.dylan5@gmail.com>
2019-04-29 12:41:36 -0400

    updated list and query

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -20,6 +20,14 @@ connection.connect(function(err) {
 
   connection.query("SELECT * FROM products", function(err, res) {
     if (err) throw err;
+    for (var i = 0; i < res.length; i++){
+        console.log(res[i].id)
+        console.log(res[i].product_name)
+        console.log(res[i].department_name)
+        console.log(res[i].price)
+        console.log(res[i].stock_quantity)
+        console.log("\n")
+    }
     console.log(res);
     console.log(res[0].id);
     connection.end();
